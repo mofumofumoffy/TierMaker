406cb9aa57f14783d65f5fa3f20b0f7016d02807
Moffy5612 <moffy3318rami@gmail.com>
2026-03-05 18:46:25 +0900

    権利表記のブラッシュアップ

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,9 @@ export default function RootLayout({
         <main className="appMain">{children}</main>
         <footer style={{ padding: "16px", color: "#374151", fontSize: "12px", lineHeight: 1.6 }}>
           <div>(C)hototogisu2003 All rights reserved.</div>
+          <div>当サイトは非公式のファンツールであり、mixi Inc.とは一切関係ありません。</div>
           <div>
-            当サイト上で使用しているゲーム アセットの全ての権利は、mixi Inc.に帰属します。その他の知的財産権は、各権利所有者に帰属します。
+            当サイト上で使用しているゲームの画像・名称・その他のアセットの著作権および商標権は、mixi Inc.に帰属します。その他、当サイトの知的財産権は、各権利所有者に帰属します。
           </div>
         </footer>
       </body>
